22de20f42f5399e1580e4608f16d447d933b2d22
Jianbang Yang <yangjianbang112@gmail.com>
2023-12-20 07:01:08 +0000

    hook xpu runtime api

--- lib/cuda_mock.cpp
+++ lib/cuda_mock.cpp
@@ -7,11 +7,131 @@
 #include "backtrace.h"
 #include "cuda_op_tracer.h"
 #include "hook.h"
+#include <stdint.h>
+#include <map>
+#include <mutex>
+
+#include "backtrace.h"
+#include "cuda_op_tracer.h"
+#include "hook.h"
 #include "logger/logger.h"
 
 std::jmp_buf log_jump_buffer = {{}};
 
 #ifdef __cplusplus
+
+namespace {
+
+typedef int (*XpuMallocPtr)(void**, uint64_t, int);
+typedef int (*XpuFreePtr)(void*);
+typedef int (*XpuCurrentDevicePtr)(int* devid);
+XpuMallocPtr xpu_malloc_fn_ptr = nullptr;
+XpuFreePtr xpu_free_fn_ptr = nullptr;
+XpuCurrentDevicePtr xpu_current_device_fn_ptr = nullptr;
+
+std::mutex xpu_malloc_mutex;
+std::vector<uint64_t> gm_allocated(8, 0);
+std::vector<uint64_t> l3_allocated(8, 0);
+std::vector<uint64_t> gm_allocated_peak(8, 0);
+std::vector<uint64_t> l3_allocated_peak(8, 0);
+
+std::vector<std::map<void*, std::pair<int, uint64_t>>> allocated_map(8);
+
+int recorded_xpu_malloc(void** pdevptr, uint64_t sz, int kind) {
+    std::lock_guard<std::mutex> lock(xpu_malloc_mutex);
+    int current_devid = 0;
+    int r = xpu_current_device_fn_ptr(&current_devid);
+    if (r != 0) {
+        return r;
+    }
+
+    r = xpu_malloc_fn_ptr(pdevptr, sz, kind);
+    if (r != 0) {
+        LOG(WARN) << "Device " << current_devid << " malloc failed, size: " << sz << ", kind: " << kind << " gm allocated: " << gm_allocated[current_devid];
+        return r;
+    }
+
+    if (kind == 0) {
+        gm_allocated[current_devid] += sz;
+        gm_allocated_peak[current_devid] = std::max(gm_allocated_peak[current_devid], gm_allocated[current_devid]);
+    } else if (kind == 1) {
+        l3_allocated[current_devid] += sz;
+        l3_allocated_peak[current_devid] = std::max(l3_allocated_peak[current_devid], l3_allocated[current_devid]);
+    }
+
+    allocated_map[current_devid][*pdevptr] = std::make_pair(current_devid, sz);
+
+    LOG(WARN) << "Device " << current_devid << " malloc size: " << sz << ", kind: " << kind << ", peak: " << gm_allocated_peak[current_devid];
+
+    return r;
+}
+
+int recorded_xpu_free(void* devptr) {
+    std::lock_guard<std::mutex> lock(xpu_malloc_mutex);
+    int current_devid = 0;
+    int r = xpu_current_device_fn_ptr(&current_devid);
+    if (r != 0) {
+        return r;
+    }
+
+    r = xpu_free_fn_ptr(devptr);
+    if (r != 0) {
+        return r;
+    }
+
+    auto it = allocated_map[current_devid].find(devptr);
+    if (it == allocated_map[current_devid].end()) {
+        return r;
+    }
+
+    int type = it->second.first;
+    uint64_t sz = it->second.second;
+
+    if (type == 0) {
+        gm_allocated[current_devid] -= sz;
+    } else if (type == 1){
+        l3_allocated[current_devid] -= sz;
+    }
+    allocated_map[current_devid].erase(it);
+    return r;
+}
+
+struct DeviceMallocFreeHook : public hook::HookInstallerWrap<DeviceMallocFreeHook> {
+    bool targetLib(const char* name) {
+        // return strstr(name, "libpaddle.so") || strstr(name, "libxpuapi.so") || strstr(name, "libphi.so");
+        return !strstr(name, "libxpurt.so.1") && !strstr(name, "libxpurt.so");
+    }
+    bool targetSym(const char* name) {
+        return strstr(name, "xpu_malloc") || strstr(name, "xpu_free") || strstr(name, "xpu_current_device") /*|| strstr(name, "xpu_wait")*/;
+    }
+    void* newFuncPtr(const hook::OriginalInfo& info) {
+        if (strstr(curSymName(), "xpu_malloc")) {
+            LOG(WARN) << info.libName << '\t' << "xpu_malloc";
+            if (!xpu_malloc_fn_ptr) {
+                xpu_malloc_fn_ptr = (XpuMallocPtr)info.oldFuncPtr;
+            }
+            return reinterpret_cast<void*>(&recorded_xpu_malloc);
+        } else if (strstr(curSymName(), "xpu_free")) {
+            LOG(WARN) << info.libName << '\t' << "xpu_free";
+            if (!xpu_free_fn_ptr) {
+                xpu_free_fn_ptr = (XpuFreePtr)info.oldFuncPtr;
+            }
+            return reinterpret_cast<void*>(&recorded_xpu_free);
+        } else if (strstr(curSymName(), "xpu_current_device")) {
+            LOG(WARN) << info.libName << '\t' << "xpu_current_device";
+            if (!xpu_current_device_fn_ptr) {
+                xpu_current_device_fn_ptr = (XpuCurrentDevicePtr)info.oldFuncPtr;
+            }
+            return info.oldFuncPtr;
+        }
+        CHECK(0, "error name");
+        return nullptr;
+    }
+
+    void onSuccess() {}
+};
+
+}
 
 extern "C" {
 
@@ -20,6 +140,8 @@ void dh_initialize() {
     hook::HookInstaller hookInstaller = trace::getHookInstaller();
     hook::install_hook(hookInstaller);
     // hook::install_hook();
+    static auto install_wrap = std::make_shared<DeviceMallocFreeHook>();
+    install_wrap->install();
 }
 
 static void* oldFuncAddr = nullptr;
